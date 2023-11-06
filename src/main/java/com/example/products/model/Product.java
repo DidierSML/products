ab125d9cd26a8aba96038e9a9a665b6859055ca3
DidierSML <didiersmaldonad@gmail.com>
2023-11-06 14:57:41 -0500

    First Commit "Products Project"

---
+++ src/main/java/com/example/products/model/Product.java
@@ -0,0 +1,18 @@
+package com.example.products.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    private int id;
+    private String name;
+    private int cantidad;
+    private double precio;
+
+
+}
