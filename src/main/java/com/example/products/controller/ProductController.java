55fca98d83913e380e5d946c5393e298ecc40f24
DidierSML <didiersmaldonad@gmail.com>
2023-11-08 10:10:53 -0500

    Adding ProductController methods

--- src/main/java/com/example/products/controller/ProductController.java
+++ src/main/java/com/example/products/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.example.products.controller;
 
+import com.example.products.model.Product;
 import com.example.products.service.ProductService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/Products")
@@ -11,6 +14,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private final ProductService productService;
+
+    @PostMapping("saveProduct")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product saveProduct (@RequestBody Product product){
+        return productService.saveProduct(product);
+    }
+
+    @GetMapping("getAllProducts")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> getAllProducts (){
+        return productService.getAllProduts();
+    }
+
+    @GetMapping("getProductById/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Product getProduct (@PathVariable Integer id){
+        return productService.findProductById(id);
+    }
+
+    @PutMapping("updateProduct/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Product updateProduct (@PathVariable Integer id, @RequestBody Product product){
+        return productService.updateProduct(id,product);
+    }
+
+    @DeleteMapping("deleteProduct/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public String deleteProduct (@PathVariable Integer id){
+        return productService.deleteProduct(id);
+    }
 
 
 
